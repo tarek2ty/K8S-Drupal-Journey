c46f62fda38d3a9768c519244b3195a792226fc5
Tarek <doda7765@gmail.com>
2025-11-06 14:19:14 +0200

    Auto Scale the app based on utilization

---
+++ drupal/scale-drupal/settings.php
@@ -0,0 +1,14 @@
+<?php
+$databases['default']['default'] = [
+  'database' => 'drupal',
+  'username' => 'drupal',
+  'password' => 'drupal',
+  'prefix' => '',
+  'host' => 'mariadb',
+  'port' => '3306',
+  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
+  'driver' => 'mysql',
+];
+$settings['hash_salt'] = 'OTk4MTYzYWI4N2E2MGIxNjlmYmQ2MTA4';
+$settings['trusted_host_patterns'] = ['^.+$'];
+$settings['config_sync_directory'] = 'sites/default/files/config_OTk4MTYzY';
